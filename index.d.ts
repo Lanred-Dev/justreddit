35a210f366554b375066d73ce4d642586739c2a9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2021-11-30 20:07:04 -0600

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
-export default function (Reddit: string, Sort_Type?: string): Promise<string>;
-export default function (Reddit: string, Sort_Type?: string, Limit?: number): Promise<string>;
+export function Post (Reddit: string, Sort_Type?: string, Limit?: number): Promise<>;
+export function Image (Reddit: string, Sort_Type?: string, Limit?: number): Promise<string>;
+export function SubReddit (Sort_Type, Limit): Promise<>;
 export declare type Limit = number;
 export declare type Reddit = string;
 export declare type Sort_Type = "top" | "new";
