4f0d033a36c2b807dcac7148900a344e9b73b94d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2022-05-19 21:16:40 -0500

    outdated

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export function Post (Reddit: string, Sort_Type?: string, Limit?: number): Promise<>;
-export function Image (Reddit: string, Sort_Type?: string, Limit?: number): Promise<string>;
-export function SubReddit (Sort_Type, Limit): Promise<>;
-export declare type Limit = number;
-export declare type Reddit = string;
-export declare type Sort_Type = "top" | "new";
