fdf7085101e1b7756cf38f21e62446a6572ba3f5
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-05-06 20:50:00 -0500

    feat: add new randomSubreddit method and fix RedditSubreddit type

--- src/Subreddit.ts
+++ src/Subreddit.ts
@@ -1,7 +1,7 @@
 import fetch from "./utils/fetch";
 
 interface RedditSubreddit {
-    type: string;
+    kind: "t5";
     data: {
         user_flair_background_color: string | null;
         submit_text_html: string;
@@ -108,46 +108,54 @@ interface RedditSubreddit {
     };
 }
 
-export interface Subreddit {
-    id: string,
-    url: string,
-    language: string,
-    name: string,
-    description: string,
-    nsfw: boolean,
-    subscribers: number,
-    created: Date,
-    usersOnline: number,
-    communityReviewed: boolean,
-    allOriginalContent: boolean,
-    colors: {
-        primary: string,
-        key: string
-    },
-    banners: {
-        desktop: string,
-        mobile: string,
-    },
-    allows: {
-        posting: boolean,
-        emojis: boolean,
-        images: boolean,
-        videos: boolean,
-        polls: boolean,
-        videogifs: boolean,
-        discovery: boolean,
-        galleries: boolean,
-        mediaInComments: boolean,
-        predictions: boolean,
-        talks: boolean,
-        followers: boolean
-    }
+interface RedditPopularSubredditsResponse {
+    kind: "Listing";
+    data: {
+        after: string | null;
+        dist: number;
+        modhash: string | null;
+        geo_filter: string | null;
+        children: RedditSubreddit[];
+    };
 }
 
-export async function get(subreddit: string): Promise<Subreddit> {
-    const response: RedditSubreddit = await fetch(subreddit);
-    const { id, url, display_name, description, over18, lang, active_user_count, allow_images, allow_videos, allow_polls, allow_videogifs, allow_discovery, allow_galleries, should_show_media_in_comments_setting, allow_predictions, allow_talks, accept_followers, subscribers, community_reviewed, emojis_enabled, restrict_posting, all_original_content, created, banner_background_image, mobile_banner_image, primary_color, key_color } = response.data;
+export interface Subreddit {
+    id: string;
+    url: string;
+    language: string;
+    name: string;
+    description: string;
+    nsfw: boolean;
+    subscribers: number;
+    created: Date;
+    usersOnline: number;
+    communityReviewed: boolean;
+    allOriginalContent: boolean;
+    colors: {
+        primary: string;
+        key: string;
+    };
+    banners: {
+        desktop: string;
+        mobile: string;
+    };
+    allows: {
+        posting: boolean;
+        emojis: boolean;
+        images: boolean;
+        videos: boolean;
+        polls: boolean;
+        videogifs: boolean;
+        discovery: boolean;
+        galleries: boolean;
+        mediaInComments: boolean;
+        predictions: boolean;
+        talks: boolean;
+        followers: boolean;
+    };
+}
 
+function parseSubreddit({ id, url, display_name, description, over18, lang, active_user_count, allow_images, allow_videos, allow_polls, allow_videogifs, allow_discovery, allow_galleries, should_show_media_in_comments_setting, allow_predictions, allow_talks, accept_followers, subscribers, community_reviewed, emojis_enabled, restrict_posting, all_original_content, created, banner_background_image, mobile_banner_image, primary_color, key_color }: RedditSubreddit["data"]): Subreddit {
     return {
         id,
         url: `https://www.reddit.com/${url}`,
@@ -162,7 +170,7 @@ export async function get(subreddit: string): Promise<Subreddit> {
         allOriginalContent: all_original_content,
         colors: {
             primary: primary_color,
-            key: key_color
+            key: key_color,
         },
         banners: {
             desktop: banner_background_image,
@@ -180,7 +188,18 @@ export async function get(subreddit: string): Promise<Subreddit> {
             mediaInComments: should_show_media_in_comments_setting,
             predictions: allow_predictions,
             talks: allow_talks,
-            followers: accept_followers
-        }
+            followers: accept_followers,
+        },
     };
+}
+
+export async function subreddit(subreddit: string): Promise<Subreddit> {
+    const response: RedditSubreddit = await fetch(subreddit);
+    return parseSubreddit(response.data);
+}
+
+export async function randomSubreddit(): Promise<Subreddit> {
+    const response: RedditPopularSubredditsResponse = await fetch("subreddits/popular");
+    const subreddits: RedditSubreddit[] = response.data.children.filter((subreddit) => subreddit.kind === "t5");
+    return parseSubreddit(subreddits[Math.floor(Math.random() * subreddits.length)].data);
 }
