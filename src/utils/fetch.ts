a5731ef5ec42a3cb667c3526aa38de46e215527f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-10-13 12:30:16 -0500

    Ensure consistency with fetch error messages

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,10 +4,10 @@ interface RedditError {
 }
 
 export default async function fetch(path: string, prefix: "user" | "r" = "r"): Promise<any> {
-    path = `${prefix}/${path}.json`
+    path = `${prefix}/${path}.json`;
     const response: Response = await fetch(`https://www.reddit.com/${path}`);
 
-    if (!response.ok || response.status !== 200) throw new Error(`Could not fetch data from path (${path}): ${response.statusText}`);
+    if (!response.ok || response.status !== 200) throw new Error(`Could not fetch data from path (${path}) with ${response.status}`);
 
     const body: RedditError | any = await response.json();
 
