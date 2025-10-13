b4daf1050d70f65ad99751df9e336ba2f2dc8957
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2025-10-13 12:38:32 -0500

    Rename fetch to fetchEndpoint and add doc comment

---
+++ src/utils/fetchEndpoint.ts
@@ -0,0 +1,22 @@
+/**
+ * A simple fetch utility to get data from Reddit's API
+ *
+ * @param path
+ * @param prefix
+ * @returns The data from the endpoint
+ */
+export default async function fetchEndpoint(path: string, prefix: "user" | "r" = "r"): Promise<any> {
+    // The Reddit API uses .json at the end of paths to return JSON data
+    path = `${prefix}/${path}.json`;
+
+    // NOTE: www.reddit.com is used because using reddit.com redirects to www anyways
+    const response: Response = await fetch(`https://www.reddit.com/${path}`);
+
+    if (!response.ok || response.status !== 200) throw new Error(`Could not fetch data from path (${path}) with ${response.status}`);
+
+    const body = await response.json();
+
+    if ("error" in body && "message" in body) throw new Error(`Could not fetch data from path (${path}) with '${body.message}' and error '${body.error}'`);
+
+    return body;
+}
