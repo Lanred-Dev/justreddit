481472ff1cb61b92a85f080fdde61e3f3d701cf3
Landon Redmond <landon.redmond0@gmail.com>
2025-10-25 13:01:13 -0500

    Improve doc comment and ensure consistency with error messages

--- src/utils/fetchEndpoint.ts
+++ src/utils/fetchEndpoint.ts
@@ -12,19 +12,19 @@ export default async function fetchEndpoint(
     // The Reddit API uses .json at the end of paths to return JSON data
     path = `${prefix ? `${prefix}/` : ""}${path}.json`;
 
-    // NOTE: www.reddit.com is used because using reddit.com redirects to www anyways
+    // reddit.com redirects to www.reddit.com, so just use that directly
     const response: Response = await fetch(`https://www.reddit.com/${path}`);
 
     if (!response.ok || response.status !== 200)
         throw new Error(
-            `Could not fetch data from ${path} with status ${response.status} ${response.statusText}`
+            `Could not fetch data from '${path}' with status ${response.status} ${response.statusText}`
         );
 
     const body = await response.json();
 
     if ("error" in body && "message" in body)
         throw new Error(
-            `Could not fetch data from ${path} with '${body.message}' and error '${body.error}'`
+            `Could not fetch data from '${path}' with status ${body.error} ${body.message}`
         );
 
     return body;
